7b8843a74883029f09ceaa5c155d689cbc45c1d2
Hayley <hayley@pixielabs.io>
2022-09-07 11:01:45 +0100

    configure sign up page

--- App.js
+++ App.js
@@ -36,4 +36,42 @@ function App() {
   )
 }
 
-export default withAuthenticator(App)
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Username',
+      key: 'preferred_username',
+      required: true,
+      displayOrder: 2,
+      type: 'string'
+    },
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 3,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 4,
+      type: 'password'
+    },
+  ]
+};
+
+export default withAuthenticator(App, {
+  signUpConfig: signUpConfig
+})
